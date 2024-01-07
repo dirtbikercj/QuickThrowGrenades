0135d7950c8bca7756dc52d2689ac29e0ea7d7c8
dirtbikercj <dirtbikercj@gmail.com>
2024-01-07 17:47:14 -0500

    Add project files.

---
+++ Patches/GrenadePatch.cs
@@ -0,0 +1,27 @@
+﻿using Aki.Reflection.Patching;
+using EFT;
+using EFT.UI;
+using System.Linq;
+using System.Reflection;
+
+namespace QuickThrowGrenades.Patches
+{
+    internal class GrenadePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(Player).GetMethods().First(m =>
+                m.Name == "SetInHands" && m.GetParameters()[0].Name == "throwWeap");
+        }
+
+        [PatchPrefix]
+        private static bool Prefix(Player __instance, GrenadeClass throwWeap)
+        {
+            if (Plugin.MainPlayer != null && Plugin.Enable.Value)
+            {
+                __instance.SetInHandsForQuickUse(throwWeap, null);
+            }
+            return !Plugin.Enable.Value;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿using EFT;
+using System;
+using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+using DrakiaXYZ.VersionChecker;
+using QuickThrowGrenades.Patches;
+using Comfort.Common;
+
+namespace QuickThrowGrenades
+{
+    [BepInPlugin("com.dirtbikercj.QuickThrowGrenades", "Quick Throw Grenades", "1.0.0")]
+    internal class Plugin : BaseUnityPlugin
+    {
+        public const int TarkovVersion = 26535;
+
+        internal static ConfigEntry<bool> Enable;
+
+        internal static Player MainPlayer = null;
+
+        private void Awake()
+        {
+            if (!VersionChecker.CheckEftVersion(Logger, Info, Config))
+            {
+                throw new Exception("Invalid EFT Version");
+            }
+
+            Enable = Config.Bind(
+               "General",
+               "Quick Throw Grenades",
+               false,
+               "Instantly throw grenades");
+
+            new GrenadePatch().Enable();
+        }
+
+        private void Update()
+        {
+            if (Singleton<GameWorld>.Instantiated && MainPlayer == null)
+            {
+                MainPlayer = Singleton<GameWorld>.Instance.MainPlayer;
+            }
+        }
+    }
+}
